d4a9e257d0dd2f198727b81af4ed6ccb569063b0
Roller23 <roller47@wp.pl>
2020-12-12 18:42:15 +0100

    Added missing lights-window

--- src/nightwatch/RadioController.java
+++ src/nightwatch/RadioController.java
@@ -5,8 +5,21 @@ import javafx.scene.control.Label;
 
 public class RadioController {
   private @FXML Label knob;
+  private double dragStart;
+  private double volume = 0;
 
   public void initialize() {
     System.out.println("Radio controller initialized");
+    knob.setOnMousePressed(e -> {
+      dragStart = e.getScreenX();
+    });
+    knob.setOnMouseDragged(e -> {
+      double delta = e.getScreenX() - dragStart;
+      volume += delta / 300 / 15;
+      if (volume > 0.8) volume = 0.8;
+      if (volume < -0.8) volume = -0.8;
+      int deg = (int)Math.floor(180 * volume);
+      knob.setStyle("-fx-rotate: " + deg + ";");
+    });
   }
 }
