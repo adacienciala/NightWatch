9630a5865561700e9ac85d3b49c5a5de867f05ff
Roller23 <roller47@wp.pl>
2020-12-17 15:32:37 +0100

    Connected the label with the knob

--- src/nightwatch/radio/RadioController.java
+++ src/nightwatch/radio/RadioController.java
@@ -5,8 +5,10 @@ import javafx.scene.control.Label;
 
 public class RadioController {
   @FXML private Label knob;
+  @FXML private Label hertz;
   private double dragStart;
   private double volume = 0;
+  private int value = 0;
 
   public void initialize() {
     System.out.println("Radio controller initialized");
@@ -18,8 +20,12 @@ public class RadioController {
       volume += delta / 300 / 15;
       if (volume > 0.8) volume = 0.8;
       if (volume < -0.8) volume = -0.8;
+      double shifted = volume + 0.8;
+      double percent = shifted / 1.6;
+      value = (int)(150.0 * percent);
       int deg = (int)Math.floor(180 * volume);
       knob.setStyle("-fx-rotate: " + deg + ";");
+      hertz.setText(value + "Hz");
     });
   }
 }
